ca8410d789743c5132e2df23c7c078463a684ef6
Leandro_Lenovo <Leandro_Lenovo@DESKTOP-7BVJ92O>
2024-02-27 10:37:11 -0300

    Final

--- ProjetoFuncoes/src/pacote_codigo/ClassePrincipal.java
+++ ProjetoFuncoes/src/pacote_codigo/ClassePrincipal.java
@@ -2,19 +2,16 @@ package pacote_codigo;
 
 public class ClassePrincipal {
 	
-	public static int ImprimirComRetornoEParametro(int a, int b){
-		
-		return a + b;
-		
+	public static int ImprimirComRetornoEParametro(int a, int b){   //public é o modificador de acesso e o int é o tipo do return
+		return a + b; // retorna um int
 	}
 	
 	public static void main(String[] args) {
-		
 		int y = 3;
 		int z = 7;
 		
 		int x;
-		x = ImprimirComRetornoEParametro(y, z);
+		x = ImprimirComRetornoEParametro(y, z); // Passando dois parâmetros.
 
 	}
 
