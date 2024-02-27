ae9b368d1ad2313d4f798b9c89272039b5f6fe7c
projetoaulasingles <161381042+projetoaulasingles@users.noreply.github.com>
2024-02-27 10:15:34 -0300

    Adicionado código-fonte
    Adicionada a primeira função.

---
+++ ProjetoFuncoes/src/pacote_codigo/ClassePrincipal.java
@@ -0,0 +1,17 @@
+package pacote_codigo;
+
+public class ClassePrincipal {
+	
+	public static void Imprimir() {
+		
+		System.out.println("Sem parâmetro e sem retorno.");
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Imprimir();
+
+	}
+
+}
